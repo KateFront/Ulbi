676ad83d6b61ae0bcff2c8d3eabfe7612f6bce1a
KateFront <ekaterina.petrushenko.94@gmail.com>
2023-09-08 11:28:49 +0300

    add Feature-Sliced Design

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
-import {render} from "react-dom";
-import App from "./App";
-import {BrowserRouter} from "react-router-dom";
-import ThemeProvider from "./styles/theme/ThemeProvider";
 import React from "react";
+import {render} from "react-dom";
+import App from "./app/App";
+import {BrowserRouter} from "react-router-dom";
+import {ThemeProvider} from "app/providers";
 
 render(
     <BrowserRouter>
