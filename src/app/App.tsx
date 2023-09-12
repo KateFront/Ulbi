24a4547d9622ef09b890cbd55f4ce4ef29132c75
KateFront <ekaterina.petrushenko.94@gmail.com>
2023-09-12 16:16:31 +0300

    add sidebar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import {Link} from "react-router-dom";
 import {classNames} from "shared/lib/classNames/classNames";
 import {useTheme} from "app/providers";
 import './styles/index.scss';
 import {AppRouter} from "app/providers/router";
 import {Navbar} from "widgets/Navbar";
+import {Sidebar} from "widgets/Sidebar";
 
 
 const App = () => {
@@ -13,8 +13,10 @@ const App = () => {
     return (
         <div className={classNames('app', {}, [theme])}>
             <Navbar/>
-            <AppRouter/>
-
+            <div className={'content-page'}>
+                <Sidebar/>
+                <AppRouter/>
+            </div>
         </div>
     );
 };
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -11,7 +11,7 @@ interface NavbarProps {
 export const Navbar = ({className}: NavbarProps) => {
     return (
         <div className={classNames(cls.Navbar, {}, [className])}>
-            <ThemeSwitcher/>
+
             <div className={cls.links}>
                 <AppLink
                     theme={AppLinkTheme.SECONDARY} to={'/'} className={cls.mainLink}>
