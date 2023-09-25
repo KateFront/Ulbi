3d82eea3366468c3d5d7e573465b2e9d5a7bdd03
KateFront <ekaterina.petrushenko.94@gmail.com>
2023-09-25 11:46:03 +0300

    add bundle analyzer

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,10 +1,11 @@
-import HTMLWebpackPlugin from "html-webpack-plugin";
-import webpack from "webpack";
-import {BuildOptions} from "./types/config";
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
-import ReactRefreshPlugin from "@pmmmwh/react-refresh-webpack-plugin";
+import HTMLWebpackPlugin from 'html-webpack-plugin';
+import webpack from 'webpack';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+import ReactRefreshPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+import { BuildOptions } from './types/config';
 
-export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPluginInstance[] {
+export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] {
     return [
         new HTMLWebpackPlugin({
             template: paths.html,
@@ -19,5 +20,8 @@ export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPlugi
         }),
         new webpack.HotModuleReplacementPlugin(),
         new ReactRefreshPlugin(),
-    ]
+        new BundleAnalyzerPlugin({
+            openAnalyzer: false,
+        }),
+    ];
 }
