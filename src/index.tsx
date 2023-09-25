1de8ae559f12c9e617d12a1a50912da28c5959b8
KateFront <ekaterina.petrushenko.94@gmail.com>
2023-09-25 11:29:15 +0300

    add error boundary

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'app/providers';
+import { ErrorBoundary } from 'app/providers/ErrorBoundary';
 import App from './app/App';
 import 'shared/config/i18n/i18n';
 
@@ -9,8 +10,10 @@ const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
     <BrowserRouter>
-        <ThemeProvider>
-            <App />
-        </ThemeProvider>
+        <ErrorBoundary>
+            <ThemeProvider>
+                <App />
+            </ThemeProvider>
+        </ErrorBoundary>
     </BrowserRouter>,
 );
