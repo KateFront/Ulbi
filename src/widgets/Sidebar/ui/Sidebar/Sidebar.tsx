bec4249303c891e7dfa910f3c3d4d8a1fb2ceabb
kfront <kfront@gmail.com>
2023-10-05 13:00:05 +0300

    fix toggle button

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,29 +1,31 @@
-import { classNames } from 'shared/lib/classNames/classNames';
-import React, { useState } from 'react';
-import { ThemeSwitcher } from 'shared/ui/ThemeSwithcer';
-import { LangSwitcher } from 'widgets/LangSwitcher/LangSwitcher';
-import { Button } from 'shared/ui/Button/Button';
+import {classNames} from 'shared/lib/classNames/classNames';
+import React, {useState} from 'react';
+import {ThemeSwitcher} from 'shared/ui/ThemeSwithcer';
+import {LangSwitcher} from 'widgets/LangSwitcher/LangSwitcher';
+import {Button} from 'shared/ui/Button/Button';
 import cls from './Sidebar.module.scss';
+import {useTranslation} from "react-i18next";
 
 interface SidebarProps {
     className?: string
 }
 
-export const Sidebar = ({ className }: SidebarProps) => {
+export const Sidebar = ({className}: SidebarProps) => {
     const [collapsed, setCollapsed] = useState(false);
-
+    const {t} = useTranslation();
     const onToggle = () => {
         setCollapsed((prev) => !prev);
     };
     return (
         <div
-            className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [className])}
+            className={classNames(cls.Sidebar, {[cls.collapsed]: collapsed}, [className])}
         >
-            {/* eslint-disable-next-line i18next/no-literal-string */}
-            <Button type="button" onClick={onToggle}>toggle</Button>
+            <Button type="button" onClick={onToggle}>
+                {t('Переключить')}
+            </Button>
             <div className={cls.switchers}>
-                <ThemeSwitcher />
-                <LangSwitcher className={cls.lang} />
+                <ThemeSwitcher/>
+                <LangSwitcher className={cls.lang}/>
             </div>
         </div>
     );
