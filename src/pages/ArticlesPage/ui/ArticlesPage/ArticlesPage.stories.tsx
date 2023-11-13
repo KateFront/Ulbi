e204226e6b7612f2a9b64504531ebc769f070fe2
kfront <kfront@gmail.com>
2023-11-13 12:31:04 +0300

    add article page

---
+++ src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.stories.tsx
@@ -0,0 +1,15 @@
+import { Meta, StoryObj } from '@storybook/react';
+import ArticlesPage from './ArticlesPage';
+
+const meta = {
+    title: 'pages/ArticlesPage',
+    component: ArticlesPage,
+} satisfies Meta<typeof ArticlesPage>;
+
+export default meta;
+
+type Story = StoryObj<typeof ArticlesPage>;
+
+export const Normal: Story = {
+    args: {},
+};
